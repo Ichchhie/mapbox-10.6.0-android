c65aeb165ea8de26a7acc7767a8b6834bf075205
Ichchha Moktan <ichchhamoktan07@gmail.com>
2023-01-11 10:30:05 +0545

    add 3D terrain map style

--- app/src/main/java/com/medix/myapplication/MainActivity.kt
+++ app/src/main/java/com/medix/myapplication/MainActivity.kt
@@ -3,7 +3,6 @@ package com.medix.myapplication
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.graphics.Point
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
@@ -13,6 +12,14 @@ import androidx.appcompat.content.res.AppCompatResources
 import com.mapbox.geojson.Point.fromLngLat
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.mapbox.maps.dsl.cameraOptions
+import com.mapbox.maps.extension.style.layers.properties.generated.ProjectionName
+import com.mapbox.maps.extension.style.projection.generated.projection
+import com.mapbox.maps.extension.style.sources.generated.rasterDemSource
+import com.mapbox.maps.extension.style.terrain.generated.terrain
+import com.mapbox.maps.plugin.animation.MapAnimationOptions.Companion.mapAnimationOptions
+import com.mapbox.maps.plugin.animation.camera
+import com.mapbox.maps.plugin.animation.flyTo
 import com.mapbox.maps.plugin.annotation.annotations
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
 import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
@@ -20,34 +27,60 @@ import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
 class MainActivity : AppCompatActivity() {
     private var mapView: MapView? = null
 
+    // 3D terrain
+    companion object {
+        private const val SOURCE = "TERRAIN_SOURCE"
+        private const val SKY_LAYER = "sky"
+        private const val TERRAIN_URL_TILE_RESOURCE = "mapbox://mapbox.mapbox-terrain-dem-v1"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
         mapView?.getMapboxMap()?.loadStyleUri(
-            Style.MAPBOX_STREETS
-        ) { addAnnotationToMap() }
+            Style.SATELLITE_STREETS
+        ) {
+            rasterDemSource(SOURCE) {
+                url(TERRAIN_URL_TILE_RESOURCE)
+            }
+            projection(ProjectionName.GLOBE)
+            terrain(SOURCE) {
+                exaggeration(1.5)
+            }
+            mapView!!.camera.apply {
+                mapView?.getMapboxMap()?.flyTo(cameraOptions {
+                    center(fromLngLat(85.3069725, 27.7045198))
+                    pitch(45.0)
+                    zoom(15.5)
+                    bearing(-17.6)
+                }, mapAnimationOptions {
+                    duration(12_000)
+                })
+
+            }
+        }
     }
 
     private fun addAnnotationToMap() {
-// Create an instance of the Annotation API and get the PointAnnotationManager.
+        // Create an instance of the Annotation API and get the PointAnnotationManager.
         bitmapFromDrawableRes(
-            this@MainActivity,
-            R.drawable.ic_outline_maps_ugc_24
+            this@MainActivity, R.drawable.ic_outline_maps_ugc_24
         )?.let {
             val annotationApi = mapView?.annotations
-            val pointAnnotationManager = annotationApi?.createPointAnnotationManager(mapView!!)
-// Set options for the resulting symbol layer.
+            val pointAnnotationManager = annotationApi?.createPointAnnotationManager()
+            // Set options for the resulting symbol layer.
             val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
-// Define a geographic coordinate.
+                // Define a geographic coordinate.
                 .withPoint(fromLngLat(18.06, 59.31))
-// Specify the bitmap you assigned to the point annotation
-// The bitmap will be added to map style automatically.
+                // Specify the bitmap you assigned to the point annotation
+                // The bitmap will be added to map style automatically.
                 .withIconImage(it)
-// Add the resulting pointAnnotation to the map.
+            // Add the resulting pointAnnotation to the map.
             pointAnnotationManager?.create(pointAnnotationOptions)
         }
     }
+
     private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
         convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
 
@@ -58,12 +91,11 @@ class MainActivity : AppCompatActivity() {
         return if (sourceDrawable is BitmapDrawable) {
             sourceDrawable.bitmap
         } else {
-// copying drawable object to not manipulate on the same reference
+            // copying drawable object to not manipulate on the same reference
             val constantState = sourceDrawable.constantState ?: return null
             val drawable = constantState.newDrawable().mutate()
             val bitmap: Bitmap = Bitmap.createBitmap(
-                drawable.intrinsicWidth, drawable.intrinsicHeight,
-                Bitmap.Config.ARGB_8888
+                drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888
             )
             val canvas = Canvas(bitmap)
             drawable.setBounds(0, 0, canvas.width, canvas.height)
